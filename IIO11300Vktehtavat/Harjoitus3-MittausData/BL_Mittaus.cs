7470ad9fa49bbebf6033331033c3633ad5ed4471
msilo <G8499@student.jamk.fi>
2016-01-27 11:14:06 +0200

    Harjoitus 3 added

---
+++ IIO11300Vktehtavat/Harjoitus3-MittausData/BL_Mittaus.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.IIO11300
+{
+  public class MittausData
+  {
+    private string kello;
+
+    #region PROPERTIES 
+    public string Kello
+    {
+      get { return kello; }
+      set { kello = value; }
+    }
+
+    private string mittaus;
+
+    public string Mittaus
+    {
+      get { return mittaus; }
+      set { mittaus = value; }
+    }
+
+    public MittausData()
+    {
+      this.kello = "0:00";
+      this.mittaus = "Ei dataa";
+    }
+
+    public MittausData(string kello, string mittaus)
+    {
+      this.kello = kello;
+      this.mittaus = mittaus;
+    }
+    #endregion
+    // Ylikirjoitetaan ToString metodi
+
+    public override string ToString()
+    {
+      //return base.ToString();
+      return kello + " = " + mittaus;
+    }
+
+  }
+}
---
+++ IIO11300Vktehtavat/Harjoitus3-MittausData/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using JAMK.IT.IIO11300;
+
+namespace Harjoitus3_MittausData
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      init();
+    }
+
+    // Omat ikkunaan liittyvät alustukset
+    private void init()
+    {
+      txtToday.Text = DateTime.Today.ToShortDateString();
+    }
+
+    private void btnSave_Click(object sender, RoutedEventArgs e)
+    {
+      // Luodaan uusi mittausdata olio ja näytetään se käyttäjälle
+      MittausData md = new MittausData(txtClock.Text, txtData.Text);
+      lbData.Items.Add(md);
+    }
+  }
+}
