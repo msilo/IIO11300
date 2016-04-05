19a1b691f01acb0975f71f62b539afef026647cf
msilo <G8499@student.jamk.fi>
2016-04-05 08:53:38 +0300

    Practical work added

---
+++ IIO11300HT_Siloaho/IIO11300HT_Siloaho/BLRecipes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IIO11300HT_Siloaho
+{
+  public class BLRecipes
+  {
+    public static void getAll()
+    {
+      throw new Exception("Business class");
+    }
+
+    public void staticData()
+    {
+
+    }
+  }
+}
---
+++ IIO11300HT_Siloaho/IIO11300HT_Siloaho/DBRecipes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IIO11300HT_Siloaho
+{
+  public class DBRecipes
+  {
+    public static void getAll()
+    {
+      throw new Exception("getAll nor implemented yet");
+    }
+  }
+}
