eb9f7ffae354461093ca24608d324b614f4d3edd
msilo <G8499@student.jamk.fi>
2016-06-15 16:46:22 +0300

    Solved assigment 3

---
+++ IIO11300Vktehtavat/Tehtava3/BLfiles.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Tehtava3
+{
+  public class BLfiles
+  {
+    public static List<string> GetFiles(string folder)
+    {
+      try
+      {
+        // Tarkasta onko kansiota olemassa
+        if (Directory.Exists(folder))
+        {
+          List<string> files = new List<string>();
+
+          string[] filut = Directory.GetFiles(folder);
+          foreach (string filename in filut)
+          {
+            files.Add(filename);
+          }
+
+          return files;
+        }
+        else
+        {
+          throw new DirectoryNotFoundException();
+        }
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+    }
+
+    public static void CombineFiles(string folder, List<string> files)
+    {
+      try
+      {
+        // Jos tiedosto on uusi
+        if (!System.IO.File.Exists(folder))
+        {
+
+          // Open file
+          using (StreamWriter sw = File.CreateText(folder))
+          {
+            foreach (var file in files) {
+              using (StreamReader sr = File.OpenText(file))
+              {
+                string rivi = "";
+                while ((rivi = sr.ReadLine()) != null)
+                {
+                  MessageBox.Show(rivi);
+                  sw.WriteLine(rivi);
+                }
+                  
+              }
+            }
+          }
+        }
+        // Jos tiedosto on olemassa
+        else
+        {
+          throw new FileNotFoundException();
+        }
+
+      }
+      catch (Exception)
+      {
+        throw;
+      }
+    }
+  }
+}
--- IIO11300Vktehtavat/Tehtava3/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava3/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,42 @@ namespace Tehtava3
   /// </summary>
   public partial class MainWindow : Window
   {
+    private List<string> files;
+
     public MainWindow()
     {
       InitializeComponent();
+      init();
+    }
+
+    private void init()
+    {
+      files = new List<string>();
+    }
+
+    private void btnGetFiles_Click(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        files = BLfiles.GetFiles(tbFolder.Text);
+        lbFiles.ItemsSource = files;
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+    }
+
+    private void tbCombineFiles_Click(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        BLfiles.CombineFiles(tbOutput.Text, files);
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
     }
   }
 }
